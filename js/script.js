84d18fe61a03b644ac95eefe3b09a4c616ba418f
Nilo Arthur <nilo.ribeiro@aluno.senai.br>
2026-03-11 18:10:17 -0300

    Implement user registration, user show and user delete
    Added user management functions to display, register, and remove users and passwords.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-let valor
+let usuarios= ["Nilo"]
+let senhas=["7830"]
+
+function seeUsuarios(){
+     usuarioPessoa=document.getElementById('tabela')
+
+     usuarioPessoa.innerHTML=""
+    //  usuarioPessoa.innerHTML=""   tava apagando o ja tinha
+
+    for(let i=0;i<usuarios.length;i++){
+
+        const linha =`<tr>
+            <td>${usuarios[i]}</td>
+            <td>${senhas[i]}</td>
+            <td><button type="button" class="excluir" onclick="userRemove(${i})">Excluir</button></td>
+        </tr>`
+
+        // linha.insertAdjacentHTML('beforeend',linha)
+        usuarioPessoa.insertAdjacentHTML('beforeend', linha);
+    }
+
+}
+function cadastrar(){
+   let usuario = document.getElementById('usuario').value
+   let senha = document.getElementById('senha').value
+
+   usuarios.push(usuario)
+   senhas.push(senha)
+
+   
+}
+function userRemove(ID){
+
+    usuarios.splice(ID,1)
+    senhas.splice(ID,1)
+    // .splice() exclui a partir da casa (2), (1) item
+
+    seeUsuarios()
+    // chamar a function
+}
